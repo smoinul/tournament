66dcd0e8405502c2f3a1d9f29528f7bb935fae67
smoinul <74661961+smoinul@users.noreply.github.com>
2022-08-12 01:39:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-import random
+from flask import Flask
 
-team = [
-    ["Rajib", "Xpandan", "Robiul", "Ashiq Baten", "Mushfique", "Tahshin", "Moinul", "Naser"],
-    ["Mostafa", "Tanvir", "Junayed", "Promit", "Rashed", "Rezaul", "Rafi", "Mosfik"],
-    ["Sujel", "Farhan", "Rishov", "Asif", "Hafeez", "Sayem", "Sajal", "Rasi"]
-]
+app = Flask(__name__)
 
-for playerNumber in range(0,8):
-    temp = ["1", "2", "3"]
-    for teamNumber in range(0,3):
-        temp [teamNumber]= team[teamNumber][playerNumber]
-    random.shuffle(temp)
-    for teamNumber in range(0, 3):
-        team[teamNumber][playerNumber] = temp[teamNumber]
-
-for teamNumber in range(0, 3):
-    print (team[teamNumber])
+@app.route("/")
+def index():
+    return "Congratulations, it's a web app!"
