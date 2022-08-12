53fa4b36ce55445cf77a7d7cbb77962c9c16e81b
smoinul <74661961+smoinul@users.noreply.github.com>
2022-08-12 02:25:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def index():
     result = "Teams:"
     for teamNumber in range(0, 3):
         string = ','.join(str(x) for x in team[teamNumber])
-        result + "</br>" + str(teamNumber+1)+". "+string
+        result = result + "</br>" + str(teamNumber+1)+". "+string
     return result
 
 if __name__ == "__main__":
